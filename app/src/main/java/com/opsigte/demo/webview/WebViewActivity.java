550854330b4eef564d8ea3e95afc083a38ae85f9
zhouzhiheng <1914194652@qq.com>
2019-03-11 16:34:19 +0800

    WebView2

---
+++ app/src/main/java/com/opsigte/demo/webview/WebViewActivity.java
@@ -0,0 +1,77 @@
+package com.opsigte.demo.webview;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.opsigte.demo.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = findViewById(R.id.webview);
+        // 加载本地文件 (1.在main目录下创建assets文件夹;2.使用下面格式访问)
+//        webView.loadUrl("file:///android_asset/test.html");
+
+
+        // 加载网络资源 (有些网站需要支持JavaScript才可以访问)
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
+        webView.loadUrl("https://m.baidu.com");
+
+    }
+
+    class MyWebChromeClient extends WebChromeClient{
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            setTitle(title);
+        }
+    }
+
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            // 在页面加载完毕后执行JavaScript代码
+            //webView.evaluateJavascript("javascript:alert('hello')",null);
+        }
+
+    }
+
+
+    // 在webview中点击手机返回键判断是否退出当前Activity
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+}
