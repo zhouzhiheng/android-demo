252735b4e06d24a77a984d0396ddbb859f0b1c57
zhouzhiheng <1914194652@qq.com>
2019-03-11 16:31:06 +0800

    WebView

--- app/src/main/java/com/opsigte/demo/MainActivity.java
+++ app/src/main/java/com/opsigte/demo/MainActivity.java
@@ -1,14 +1,15 @@
 package com.opsigte.demo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.opsigte.demo.gridview.GridViewActivity;
 import com.opsigte.demo.listView.ListViewActivity;
 import com.opsigte.demo.recyclerview.RecyclerActivity;
+import com.opsigte.demo.webview.WebViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnListView;
     private Button btnGridView;
     private Button btnRecyclerView;
-
+    private Button btnWebView;
 
     private void setListeners(){
         OnClick onClick = new OnClick();
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         btnListView.setOnClickListener(onClick);
         btnGridView.setOnClickListener(onClick);
         btnRecyclerView.setOnClickListener(onClick);
+        btnWebView.setOnClickListener(onClick);
 
     }
 
@@ -69,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_recycler_view:
                     intent = new Intent(MainActivity.this, RecyclerActivity.class);
                     break;
+                case R.id.btn_webview_view:
+                    intent = new Intent(MainActivity.this, WebViewActivity.class);
+                    break;
             }
             startActivity(intent);
         }
@@ -89,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         btnListView = findViewById(R.id.btn_list_view);
         btnGridView = findViewById(R.id.btn_grid_view);
         btnRecyclerView = findViewById(R.id.btn_recycler_view);
+        btnWebView = findViewById(R.id.btn_webview_view);
         setListeners();
     }
 }
